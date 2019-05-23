f84cfa3d13b41d5f0265c6e491cdc7d9c0130ca2
julio marin <jotacemarin@gmail.com>
2019-05-23 03:18:13 -0500

    feat(api-go):
    deconstructed response from resource and build a entity for this server
    
    * added functions to get Title and Logo from webpage
    * added instructions for get Owner and Country
    * retrieve json with expected structure

--- commons/resttemplatehelper.go
+++ commons/resttemplatehelper.go
@@ -16,14 +16,3 @@ func HTTPGet(url string) ([]byte, error) {
 	data, _ := ioutil.ReadAll(response.Body)
 	return data, nil
 }
-
-// HTTPGetPage : func
-func HTTPGetPage(url string) error {
-	log.Printf("request to %s", url)
-	response, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	log.Print(response)
-	return nil
-}
